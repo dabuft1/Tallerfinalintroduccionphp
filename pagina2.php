d75f22bec28fd0e27f373e32f24726d29aa4d139
David Antonio Bermudez Uribe <dabuft1@gmail.com>
2020-11-13 21:21:55 -0500

    Se agrega responsive con bootstrap para el taller final

---
+++ pagina2.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taller final php</title>
+    <link rel="stylesheet" type="text/css" href="static/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="static/css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <h1>Jericó pueblo lindo</h1>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1"><a href="index.php"> Inicio</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1" class="activo">Documental</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1"><a href="pagina3.php"> Simbolos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </div>
+
+        <div class="row">
+            <article>
+            
+            <div class="col-md-3">
+                <h2>‘Jericó, el infinito vuelo de los días’</h2>
+                
+                <p>Jericó, el infinito vuelo de los días es una película documental colombofrancesa de 2016 dirigida y escrita por Catalina Mesa. Fue estrenado en Colombia en noviembre de 2016 y fue exhibido en eventos a nivel internacional como el Festival de Cine Documental de Canadá, el Festival de Cine de Lima, el Festival Internacional de Cine Documental de Tel Aviv y el Bogotá Film Festival, además de ganar varios premios y reconocimientos.2</p>
+            </div>
+            <div class="col-md-3">
+                <h3>Trailer</h3>
+                <a href="http://compacto.coop/films/jerico-el-infinito-vuelo-de-los-dias/"> <img class="img-responsive img-rounded"  src="images/Jeric_el_infinito_vuelo_de_los_dias.jpg">
+                </a>
+            </div>
+           
+
+
+            
+                <h2>Hermosos paisajes</h2>
+                <p>“Este mi noble Jericó es bonito enclavado en el sol de la montaña, el monte azul rozando el infinito y el infinito entrando en la cabaña” Oliva Sossa de Jaramillo, 1938</p>
+            
+            <div class="col-md-1">
+                <img class="img-responsive img-rounded"  src="images/Campo.jpg">
+            </div>
+            <div class="col-md-1">
+                <img class="img-responsive img-rounded" src="images/jerico4.jpg">
+            </div>
+            <div class="col-md-1">
+                <img class="img-responsive img-rounded" src="images/jerico1.jpg">
+            </div>
+                <div class="col-md-1">
+                <img class="img-responsive img-rounded" src="images/jerico2.jpg">
+                </div>
+                <div class="col-md-1">
+                <img class="img-responsive img-rounded" src="images/jerico3.jpg">
+                </div>
+            </article>
+        </div>
+        <div id="footer">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Ven y descubre un pueblo hermoso</h3>
+            </div>
+        </div> 
+        </div>  
+    </div> 
+</body>
+</div>  
+</html>
---
+++ pagina3.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taller final php</title>
+    <link rel="stylesheet" type="text/css" href="static/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="static/css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <h1>Jericó pueblo lindo</h1>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1"><a href="index.php"> Inicio</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1" ><a href="pagina2.php"> Página 2</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-4">
+                <nav>
+                    <ul>
+                        <li id="menu1" class="activo">Simbolos</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </div>
+
+        <div class="row">
+            <article>
+            <div class="col-md-6">
+                <h2>Himno de Jericó/h2>
+                <audio controls>
+                    <source src="audio/HIMNO DE JERICÓ.mp3" ype="audio/mp3">        
+                    </audio>
+                
+            </div>
+
+
+            <div class="col-md-6">
+                <h2>Escudo.</h2>
+                <img class="img-responsive img-rounded"  src="images/Escudo.jpg">
+            </div>
+            </article>
+        </div>
+        <div id="footer">
+        <div class="row">
+            <div class="col-md-12">
+                <h3> Ven y descubre un pueblo hermoso</h3>
+            </div>
+        </div> 
+        </div>  
+    </div> 
+</body>
+</div>  
+</html>
